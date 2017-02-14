17cb212d1478eb089e449337173a7fb71112f76b
Luke Snowden <lukesnowden@Lukes-New-Mac.local>
2017-02-14 16:23:50 +0000

    added a slugify method

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -60,6 +60,20 @@ $.fn.ensphere = new function() {
 
         $(document).bind( 'click.veto', checkForNewWysiwygInstances );
 
+        /**
+         *
+         * @param text
+         * @returns {string}
+         */
+        this.slugify = function( text )
+        {
+            return text.toString().toLowerCase()
+                .replace( /\s+/g, '-' )           // Replace spaces with -
+                .replace( /[^\w\-]+/g, '' )       // Remove all non-word chars
+                .replace( /\-\-+/g, '-' )         // Replace multiple - with single -
+                .replace( /^-+/, '' )             // Trim - from start of text
+                .replace( /-+$/, '' );            // Trim - from end of text
+        };
 
         /**
          *
