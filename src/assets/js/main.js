092d901bd020682c6e63bb6602b6b7d7da99322a
Luke Snowden <lukesnowden@Lukes-New-Mac.local>
2017-11-16 09:25:04 +0000

    Added a search filter plugin as its getting used a lot throughout the modules

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,6 +4,138 @@
  */
 $.fn.ensphere = new function() {
 
+    $.fn.ajaxFilter = function( _options )
+    {
+
+        var _ajaxFilter = function( elm, options )
+        {
+
+            options = $.extend( {
+
+                filterUrl: '',
+                label: 'Start to type to filter:',
+                onSelectedItem : function( item, e, model ) {},
+                displayRow: function( item ) {
+                    return '#' + item.id + ' - ' + item.name;
+                }
+
+            }, options );
+
+            var items = {};
+
+            var uuidv4 = function()
+            {
+                return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                    return 'ID-' + v.toString(16);
+                });
+            }
+
+            var searchTemplate = '' +
+                '<div class="field c-ajax-filter">' +
+                '<label class="c-ajax-filter__label">' + options.label + '</label>' +
+                '<input type="text" class="form-control c-ajax-filter__input" />' +
+                '</div>';
+
+            var resultsTemplate = '' +
+                '<div class="c-ajax-filter__results"></div>';
+
+            var api = {
+
+                search: null,
+                searchBox: null,
+                results: null
+
+            };
+
+            var xhr;
+
+            var populateResults = function( response )
+            {
+                api.results.html('');
+                response.forEach( function( item ) {
+                    var uid = uuidv4();
+                    var text = options.displayRow( item );
+                    var resultItem = $( '<div />' ).html( text ).addClass( 'c-ajax-filter__results__item' );
+                    items[uid] = item;
+                    api.results.append( resultItem );
+                    resultItemEvents( resultItem );
+                    resultItem[0].uid = uid;
+                });
+                api.results.show();
+            };
+
+            var closeAndClearSearchBox = function()
+            {
+                api.searchBox.val( '' );
+                api.results.html( '' ).hide();
+            };
+
+            var resultItemEvents = function( elm )
+            {
+                elm.on( 'mouseenter', onItemMouseEnterHandler );
+                elm.on( 'mouseleave', onItemMouseLeaveHandler );
+                elm.on( 'click', function(e) {
+                    options.onSelectedItem( $(this), e, items[$(this)[0].uid] );
+                    closeAndClearSearchBox();
+                });
+            }
+
+            var onItemMouseEnterHandler = function(e)
+            {
+                $(this).addClass( 'c-ajax-filter__results__item--hover' );
+            };
+
+            var onItemMouseLeaveHandler = function(e)
+            {
+                $(this).removeClass( 'c-ajax-filter__results__item--hover' );
+            };
+
+            var onDocumentClickHandler = function(e)
+            {
+                var elm = $( e.target );
+                if( ! elm.parents( '.c-ajax-filter:eq(0)' ).length ) {
+                    closeAndClearSearchBox();
+                }
+            };
+
+            var onSearchBoxKeyUpHandler = function(e)
+            {
+                e.preventDefault();
+                if( xhr && xhr.readystate != 4 ) xhr.abort();
+                xhr = $.getJSON( options.filterUrl, { query: $(this).val() }, populateResults );
+            };
+
+            var bindEvents = function()
+            {
+                api.searchBox.on( 'keyup', onSearchBoxKeyUpHandler );
+                $(document).on( 'click', onDocumentClickHandler );
+            };
+
+            var init = function()
+            {
+                var search = $( searchTemplate ).appendTo( elm );
+                var results = $( resultsTemplate ).appendTo( search );
+                api.search = search;
+                api.searchBox = $( '.c-ajax-filter__input', api.search );
+                api.results = results;
+                bindEvents();
+            };
+
+            init();
+
+            return api;
+
+        };
+
+        return $(this).each( function() {
+
+            $(this)[0].ajaxFilter = new _ajaxFilter( $(this), _options );
+
+        });
+
+    };
+
     /**
      * Veto
      */
