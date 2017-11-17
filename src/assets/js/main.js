cc5c3e3adf499dfc302124d0240029cbc0d3440c
Daniel Watson <daniel.watson@purposemedia.co.uk>
2017-11-17 15:42:32 +0000

    Added a port of the properties plugin for this version of redactor as a dependency

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -93,7 +93,7 @@ $.fn.ensphere = new function() {
                 toolbarFixed : true,
                 cleanStyleOnEnter : true,
                 deniedTags : ['html', 'head', 'link', 'body', 'meta', 'script', 'style', 'applet' ],
-                plugins : [ 'mediaManager', 'scriptbuttons' ]
+                plugins : [ 'mediaManager', 'scriptbuttons', 'properties' ]
             });
         };
 
